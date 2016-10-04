2cc0be89f2ad3de38e93d009d11cd08367377c0e
Andrea Liew <andrealiew23@gmail.com>
2016-10-04 02:19:07 +0000

    get  signup form

--- app/actions.rb
+++ app/actions.rb
@@ -4,3 +4,32 @@ get '/' do
   erb(:index)
 
 end
+
+
+get '/signup' do      #user navigates to the path /signup
+  @user = User.new    #setup empty @user object
+  erb(:signup)        #render "app/views/signup.erb"
+end
+
+post '/signup' do
+  # params.to_s   #params hash allows access to our user-submitted values
+  
+  # grab user input values from params
+  email       = params[:email]
+  avatar_url  = params[:avatar_url]
+  username    = params[:username]
+  password    = params[:password]
+  
+    # instantiate a User
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
+    
+    # if user validations pass and user is saved
+  if @user.save
+    "User #{username} saved!"    
+    # return readable represenation of User object
+  
+  else
+    erb(:signup)  # when error, brings us back to the signup page
+  # display simple error message
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
     
-    belongs_to :posts
-    has_many :comments
-    has_many :likes
-    
+  belongs_to :posts
+  has_many :comments
+  has_many :likes
+  
+  # validations methods. More here: "http://guides.rubyonrails.org/active_record_validations.html"
+  validates_presence_of :email, :avatar_url, :username, :password  
+  validates_uniqueness_of :email, :username
+  
 end

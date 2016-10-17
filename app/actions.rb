08cc3c40c9c4e635f7587ec60ef4aa1cc783ca3e
Andrea Liew <andrealiew23@gmail.com>
2016-10-17 17:20:00 +0000

    like forms

--- app/actions.rb
+++ app/actions.rb
@@ -113,3 +113,20 @@ post '/comments' do
   redirect(back)
 end
 
+# CREATE ACTION FOR LIKE FORM
+post '/likes' do
+  post_id = params[:post_id]
+  
+  like = Like.new({ post_id: post_id, user_id: current_user.id })
+
+  like.save
+  
+  redirect(back)
+end
+
+# ACTION FOR MULTIPLE LIKES (DELETE)
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
+end

c289bfeaa84b338ee5e0bdc1e2ca0b7a3d01944b
Andrea Liew <andrealiew23@gmail.com>
2016-10-17 20:33:42 +0000

    get weeks/days/mins/secs ago conversions

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,11 +14,23 @@ class Post < ActiveRecord::Base
     #equivalent to calling .created_at on the object itself
     time_ago_in_minutes = time_ago_in_seconds / 60
     
-    if time_ago_in_minutes >= 60
-      "#{(time_ago_in_minutes / 60).to_i} hours ago"
-    else
-      "#{(time_ago_in_minutes)} minutes ago"
-    end
+    time_ago_in_hours = time_ago_in_minutes / 60
+    
+    time_ago_in_days = time_ago_in_hours / 24
+    
+    time_ago_in_weeks = time_ago_in_days / 7
+if time_ago_in_days >= 7
+  "#{(time_ago_in_weeks).to_i} weeks ago"
+elsif time_ago_in_hours >= 24
+  "#{(time_ago_in_days).to_i} days ago"
+elsif time_ago_in_minutes >= 60
+  "#{(time_ago_in_hours).to_i} hours ago"
+elsif time_ago_in_minutes <= 60
+"#{(time_ago_in_minutes).to_i} minutes ago"
+elsif time_ago_in_seconds <= 60
+"#{(time_ago_in_minutes).to_i} <1 minute ago"
+end
+
   end
   
   def like_count
